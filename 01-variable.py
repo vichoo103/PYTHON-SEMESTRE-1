2d6e82063033936873f5eabcbeecdd45383f0dd8
vicente diaz <vichoo103@users.noreply.github.com>
2023-04-05 17:51:07 -0400

    primer commit

---
+++ 01-variable.py
@@ -0,0 +1,15 @@
+name= "constanza"
+nombre = "vicente"
+edad= 18
+print ("hola mi nombre es" +"nombre "+"y tengo" + str(edad) + "años")
+
+nombre= "jorge"
+print ("hola mi nuevo nombre es", nombre)
+
+nombre = input("¿cual es tu nombre?: \n")
+#print("su nombre es:" ,nombre)
+print("mi nuevo nombre es:",nombre)
+
+
+
+
