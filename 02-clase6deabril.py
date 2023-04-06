22ff633758d5af891590a877553aa0654f1ae44c
vicente diaz <vichoo103@user.noreply.github.com>
2023-04-06 16:05:59 -0400

    clase 06\04

---
+++ 02-clase6deabril.py
@@ -0,0 +1,31 @@
+#01-DATOS DE TIPO NUMERICO
+estatura = 1.71
+peso = 70
+complejo = 1 +4j
+
+print("Impresion del numero complejo:",complejo)
+#operacion de aritmetica basica
+imc = peso/estatura**2
+print("mi IMC es de:",imc)
+
+#datos de tipos de carrera de caracter
+asignatura = "programacion"
+carrera = "ingenieria civil en informatica"
+print("la asignatura de",asignatura,"corresponde a la carrera de",carrera)
+
+#valores booleanos
+ampolleta = False
+interruptor = True
+
+#con type sabemos el tipo de datos que estamos tratando
+print(type(ampolleta))
+
+ampolleta = "soy una ampolleta"
+print(type(ampolleta))
+# datos de tipos array (objetos de tipo coleccion)
+estudiantes = ["matias","marco","cristobal","sebastian"]
+num = [1,2,3,4,5,6]
+print(estudiantes)
+print(num)
+
+
