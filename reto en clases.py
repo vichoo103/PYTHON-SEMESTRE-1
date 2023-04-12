d0173f1c9ba44846efb33d1e2d21e24f5bc2e9ce
vicente diaz <vichoo103@user.noreply.github.com>
2023-04-12 17:57:44 -0400

    typos de datos finsl 12-04

---
+++ reto en clases.py
@@ -0,0 +1,10 @@
+nombre = input("¿cual es tu nombre?\n")
+edad = input("¿cual es tu edad?\n")
+print(type(edad))
+total_edad = int(edad) + 20
+
+formato = f"hola mi nombre es {nombre}, tengo {str(edad)} años y en 20 tendre {str(total_edad)}años"
+print(formato)
+
+
+
