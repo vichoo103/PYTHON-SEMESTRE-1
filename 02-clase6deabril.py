73cf5c97753709794b0faeb0440489b94cfd6a21
vicente diaz <vichoo103@user.noreply.github.com>
2023-04-12 10:37:55 -0400

    clase 11-04

--- 02-clase6deabril.py
+++ 02-clase6deabril.py
@@ -1,17 +1,24 @@
 #01-DATOS DE TIPO NUMERICO
-estatura = 1.71
-peso = 70
-complejo = 1 +4j
+edad = 29 #entero
+estatura = 1.71 #real
+peso = 70 #real
+complejo = 1 +4j #complejo
 
 print("Impresion del numero complejo:",complejo)
 #operacion de aritmetica basica
 imc = peso/estatura**2
 print("mi IMC es de:",imc)
 
-#datos de tipos de carrera de caracter
+print("mi IMC es de {:.2f}".format(imc))
+
+#02-datos de tipos de cadena de caracter
 asignatura = "programacion"
 carrera = "ingenieria civil en informatica"
 print("la asignatura de",asignatura,"corresponde a la carrera de",carrera)
+print("la asignatura de",len(asignatura),"corresponde a la carrera de",len(carrera))
+
+#utilizando la funcion len (cuenta la cantidad de caracteres)
+
 
 #valores booleanos
 ampolleta = False
@@ -23,9 +30,47 @@ print(type(ampolleta))
 ampolleta = "soy una ampolleta"
 print(type(ampolleta))
 # datos de tipos array (objetos de tipo coleccion)
+
 estudiantes = ["matias","marco","cristobal","sebastian"]
 num = [1,2,3,4,5,6]
-print(estudiantes)
-print(num)
+lenguaje = ["python"]
+data= ["osorno", {"UV": "nivel bajo", "Temp °c": 15}, (-40.5725, -70.432)]
+print(data)
+print(len(data))
+print("lista de cadena de caracteres:",estudiantes)
+print("lista de numeros:",num)
+print("lista de elementos:",lenguaje)
+
+print(estudiantes.count("matias"))
+
+
+nueva_lista = list()
+print("esta es una lista vacia",nueva_lista)
+
+
+
+
+print(estudiantes[0])
+print(estudiantes[1])
+
+estudiantes[0] = "gabriela"
+print(estudiantes[0])
+
+
+data_asig = [10023, "programacion",1,True]
+
+
+cod,ramo,semestre,estado= data_asig
+print(ramo)
+
+
+print(list("python"))
+print(list(range(10)))
+print("\n")
+
+
+
+
+
 
 
