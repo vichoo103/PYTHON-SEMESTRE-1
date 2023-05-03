c98337666c2733093be6d1f1885f112f340b6fa6
vicente diaz <vichoo103@user.noreply.github.com>
2023-05-03 17:59:26 -0400

    trabajo

---
+++ retos en clases/trabajo autonomo.py
@@ -0,0 +1,42 @@
+'''
+# reto en clases n2
+# establecimiento = "universidad de los lagos"
+# print(establecimiento.split())
+estudiante = input("ingrese el nombre del estudiante: ")
+asignatura =input("ingrese el nombre de la asignatura: ")
+nota1 =float(input("ingrese la nota de laboratorio n1: "))
+nota2 =float(input("ingrese la nota de laboratorio n2: "))
+
+promedio = (nota1 * 0.3) + (nota2 * 0.7)
+
+diccionario={
+
+"nombre_del_estudiante": estudiante,
+"nombre_de_la_asignatura": asignatura,
+"nota1": nota1,
+"nota2": nota2,
+"promedio":promedio
+}
+
+print(diccionario)
+
+
+#reto en clase n 3
+
+ciudades=["santiago", "temuco", "osorno", "punta arenas"]
+indice_de_calidad_del_aire=[134, 99, 245, 50]
+
+indice_minimo = min(indice_de_calidad_del_aire)
+ciudad_minima = ciudades[indice_de_calidad_del_aire.index(indice_minimo)]
+
+indice_maximo = max(indice_de_calidad_del_aire)
+ciudad_maxima = ciudades[indice_de_calidad_del_aire.index(indice_maximo)]
+
+print("la ciudad de punta arena tiene un grado de:",indice_minimo)
+print("la ciudad osorno tiene un grado de",indice_maximo)
+'''
+
+ciudades = ["santiago", "temuco", "osorno", "punta arenas"]
+iac = [134, 99, 245, 50]
+
+
